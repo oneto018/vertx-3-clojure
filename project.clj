28d21413c9b39d8f44fe6ca0ae958e4515bcffe1
Andre Venter <andre.n.venter@gmail.com>
2015-12-12 12:49:17 +0200

    bump vertex version to 3.1.0

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [io.vertx/vertx-core "3.0.0"]
-                 [io.vertx/vertx-web "3.0.0"]
+                 [io.vertx/vertx-core "3.1.0"]
+                 [io.vertx/vertx-web "3.1.0"]
                  [camel-snake-kebab "0.3.2"]]
   :main vertx-web-clj.core
-  
+
   :global-vars {*warn-on-reflection* true})
